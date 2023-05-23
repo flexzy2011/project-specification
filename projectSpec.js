fa5fd7b98f77ddc61499e7c19b7cd35242a628d4
flexzy2011 <felixakinloye247@gmail.com>
2023-05-23 12:20:58 +0100

    my commit

---
+++ projectSpec.js
@@ -0,0 +1,47 @@
+
+/*
+Student Name : Felix Akinloye
+Name of Project : Flexzy Weather App
+
+Idea of the project : Is to create a weather application that also has google map on it.
+         when the city name of our choice is being searched on the search icon, the web page should return the city's felt temperature/humidity/wind/
+       visibility/max.temperature/min.temperature for the exact city name searched.
+      it should also show the weather forecast of the exact day searched and for the next 7days.
+
+   Main Features/Dependencies
+         Header,Main,Footer,chakra-ui/icons,chakra-ui/react,emotion/react,emotion/styled,axios,react-redux,redux,redux-thunk and typewriter-effect.
+
+       Dependencies in details
+   chakra-ui/icons : Interface icons provided by Chakra;
+      To import { RepeatIcon, Search2Icon } from "@chakra-ui/icons in the project.
+
+   chakra-ui/react : Responsive and accessible React UI components built with React and Emotion;
+   To import {
+    Button,
+    Heading,
+    SimpleGrid,
+    Text,
+    useToast,
+    VStack,
+    Box,
+    Flex,
+    Image,
+    Text
+  } from "@chakra-ui/react" in the project.
+
+   emotion/react : For Simple styling in React.
+
+    emotion/styled : For Styled API for emotion.
+
+    Axios : Promise based HTTP client for the browser and node.js;
+    To get https://api.openweathermap .
+
+    react-redux : Official React bindings for Redux;
+     To import { useDispatch, useSelector } from "react-redux"; in the project. 
+
+     Redux : Predictable state container for JavaScript apps;
+     To import { applyMiddleware, legacy_createStore } from "redux"; in the project.
+
+     redux-thunk : Thunk middleware for Redux;
+     To import thunk from "redux-thunk"; in the project.
+     */
